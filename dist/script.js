5c1689135244ccfef3715f0502e48c7228cf51f7
Mesbah Uddin <msbh214@gmail.com>
2024-03-17 06:43:12 +0600

    1st iteration is completed

--- dist/script.js
+++ dist/script.js
@@ -1,13 +1,22 @@
 // Works for accordion
 (function accordion() {
     let items = document.querySelectorAll(".faq__question")
-    let questions = document.querySelectorAll(".faq__question")
     let containers = document.querySelectorAll(".faq__item")
 
     items.forEach((item, index) => {
         item.addEventListener('click', () => {
             containers[index].classList.toggle('active')
+
+            removeActive(index)
         })
     })
+
+    function removeActive(id) {
+        containers.forEach((container, index) => {
+            if (index !==  id) {
+                container.classList.remove('active')
+            }
+        })
+    }
 
 })()
