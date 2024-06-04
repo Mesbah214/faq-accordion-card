5728fbc466117ed9576fd6ac4209e6f355db60ad
Mesbah Uddin <msbh214@gmail.com>
2024-06-05 00:38:48 +0600

    made accordion accessible

--- dist/script.js
+++ dist/script.js
@@ -1,22 +1,37 @@
 // Works for accordion
-(function accordion() {
-    let items = document.querySelectorAll(".faq__question")
-    let containers = document.querySelectorAll(".faq__item")
+// (function accordion() {
+//     let items = document.querySelectorAll('.faq__question')
+//     let containers = document.querySelectorAll('.faq__item')
 
-    items.forEach((item, index) => {
-        item.addEventListener('click', () => {
-            containers[index].classList.toggle('active')
+//     items.forEach((item, index) => {
+//         item.addEventListener('click', () => {
+//             containers[index].classList.toggle('active')
 
-            removeActive(index)
-        })
-    })
+//             removeActive(index)
+//         })
+//     })
 
-    function removeActive(id) {
-        containers.forEach((container, index) => {
-            if (index !==  id) {
-                container.classList.remove('active')
-            }
-        })
+//     function removeActive(id) {
+//         containers.forEach((container, index) => {
+//             if (index !== id) {
+//                 container.classList.remove('active')
+//             }
+//         })
+//     }
+// })()
+
+const accordionHeaders = document.querySelectorAll('[data-accordion-header]')
+
+Array.prototype.forEach.call(accordionHeaders, (accordionHeader) => {
+    let target = accordionHeader.parentElement.nextElementSibling
+    let svg = accordionHeader.firstElementChild
+    accordionHeader.onclick = () => {
+        let expanded = accordionHeader.getAttribute('aria-expanded') === 'true' || false
+        accordionHeader.setAttribute('aria-expanded', !expanded)
+        target.setAttribute('aria-hidden', expanded)
+        target.inert = expanded
+        svg.style.rotate = expanded ? '0deg' : '180deg'
+        accordionHeader.style.fontWeight = expanded ? 'normal' : 'bold'
+        target.classList.toggle('is__hidden')
     }
-
-})()
+})
